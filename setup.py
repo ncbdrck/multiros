3876d8c5bdfb8a5c021db7ac552fcf0344935856
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-07-24 17:03:17 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
+    name="multiros",
     packages=['multiros'],
     package_dir={'': 'src'},
     version='1.0.0',
 
     description="MultiROS: ROS-Based Robot Simulation Environment for Concurrent Deep Reinforcement Learning",
     url="https://github.com/ncbdrck/multiros",
-    keywords=['ROS', 'reinforcement learning', 'gazebo', 'simulation', 'robotics'],
+    keywords=['ROS', 'reinforcement learning', 'gazebo', 'simulation', 'robotics', 'gym', 'openai'],
 
     author='Jayasekara Kapukotuwa',
     author_email='j.kapukotuwa@research.ait.ie',
